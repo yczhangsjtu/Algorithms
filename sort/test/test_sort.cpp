c101110be52ab6b1fee66e898e2f1bcdf840363f
yczhang <shjdzhangyuncong@sjtu.edu.cn>
2017-09-13 16:43:55 +0800

    Implement counting sort and radix sort.

--- sort/test/test_sort.cpp
+++ sort/test/test_sort.cpp
@@ -2,6 +2,8 @@
 #include "merge_sort.hpp"
 #include "bubble_sort.hpp"
 #include "quick_sort.hpp"
+#include "counting_sort.hpp"
+#include "radix_sort.hpp"
 #include <gtest/gtest.h>
 
 TEST(InsertionSortTest, Sorted) {
@@ -90,6 +92,42 @@ TEST(RQuickSortTest, Normal) {
 	int sarray[] = {1,2,3,4,5,6};
 	int s = sizeof(array)/sizeof(array[0]);
 	rquickSort(array,s);
+	for(int i = 0; i < s; i++)
+		ASSERT_EQ(array[i],sarray[i]);
+}
+
+TEST(CountingSortTest, Sorted) {
+	int array[] =  {1,2,3,4,5,6};
+	int sarray[] = {1,2,3,4,5,6};
+	int s = sizeof(array)/sizeof(array[0]);
+	countingSort(array,s,7);
+	for(int i = 0; i < s; i++)
+		ASSERT_EQ(array[i],sarray[i]);
+}
+
+TEST(CountingSortTest, Normal) {
+	int array[] =  {4,1,3,2,6,5};
+	int sarray[] = {1,2,3,4,5,6};
+	int s = sizeof(array)/sizeof(array[0]);
+	countingSort(array,s,7);
+	for(int i = 0; i < s; i++)
+		ASSERT_EQ(array[i],sarray[i]);
+}
+
+TEST(CountingSortTest, Repetive) {
+	int array[] =  {2,5,3,0,2,3,0,3};
+	int sarray[] = {0,0,2,2,3,3,3,5};
+	int s = sizeof(array)/sizeof(array[0]);
+	countingSort(array,s,7);
+	for(int i = 0; i < s; i++)
+		ASSERT_EQ(array[i],sarray[i]);
+}
+
+TEST(RadixSortTest, Normal) {
+	int array[] =  {329,457,657,839,436,720,355};
+	int sarray[] = {329,355,436,457,657,720,839};
+	int s = sizeof(array)/sizeof(array[0]);
+	radixSort(array,s,3);
 	for(int i = 0; i < s; i++)
 		ASSERT_EQ(array[i],sarray[i]);
 }
